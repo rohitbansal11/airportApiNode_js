f660a63c743322fab7f2b8adc543f8bc4493c16d
rohit00008888 <49895610+rohit00008888@users.noreply.github.com>
2022-02-13 19:19:58 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var app = express();
+const data =require('./Data.js')
+
+app.get('/', function (req, res) {
+    res.json()
+})
+
+app.get('/country/:country' , async function (req,res){
+    let country = String(req.params.country).toLowerCase()
+   let  mydata= data?.filter((w)=>{
+
+        return String(w?.country).toLowerCase()==country
+        
+    })
+    res.json({
+        massage:'filter By counrty is here ',
+        mydata
+    })
+})
+
+app.get('/country/state/:country/:state', function(req,res){
+
+    let countryparam = String(req.params.country).toLowerCase()
+    let stateparam =String(req.params.state).toLowerCase()
+
+    let result= data?.filter((w)=>{
+        return String(w.country).toLowerCase()==countryparam && String(w.state).toLowerCase()==stateparam
+    })
+    res.json({
+        massage:'filter by country and state',
+        result 
+    })
+
+})
+
+
+
+
+aap.get('/country/state/city/:country/:state/:city' , function (req, res) {
+
+    let countryparam = String(req.params.country).toLowerCase()
+    let stateparam = String(req.params.state).toLowerCase()
+    let cityparam =String(req.params.city).toLowerCase()
+
+    let result = data?.filter((w) => {
+        return String(w.country).toLowerCase() == countryparam && String(w.state).toLowerCase() == stateparam && String(W.city)==cityparam
+    })
+    res.json({
+        massage: 'filter by country and state',
+        result
+    })
+
+})
+
+
+
+
+var server = app.listen(8081, function () {
+    var host = server.address('localhost').address
+    var port = server.address().port
+
+    console.log("Example app listening at http://%s:%s", host, port)
+})
