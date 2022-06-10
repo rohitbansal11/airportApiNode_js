42cec6c0e8efa376cfc774cf163a74b8265313cb
unknown <rbansal000888@gmail.com>
2022-06-10 10:59:43 +0530

    new file is added

--- server.js
+++ server.js
@@ -1,67 +1,61 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-const data =require('./Data.js')
+const data = require("./Data.js");
 
-app.get('/', function (req, res) {
-    res.json({
-    
-    massage:'api is running'
-    })
-})
+app.get("/", function (req, res) {
+  res.json({
+    massage: "api is running",
+  });
+});
 
-app.get('/country/:country' , async function (req,res){
-    let country = String(req.params.country).toLowerCase()
-   let  mydata= data?.filter((w)=>{
+app.get("/country/:country", async function (req, res) {
+  let country = String(req.params.country).toLowerCase();
+  let mydata = data?.filter((w) => {
+    return String(w?.country).toLowerCase() == country;
+  });
+  res.json({
+    massage: "filter By counrty is here ",
+    mydata,
+  });
+});
 
-        return String(w?.country).toLowerCase()==country
-        
-    })
-    res.json({
-        massage:'filter By counrty is here ',
-        mydata
-    })
-})
+app.get("/country/state/:country/:state", function (req, res) {
+  let countryparam = String(req.params.country).toLowerCase();
+  let stateparam = String(req.params.state).toLowerCase();
 
-app.get('/country/state/:country/:state', function(req,res){
+  let result = data?.filter((w) => {
+    return (
+      String(w.country).toLowerCase() == countryparam &&
+      String(w.state).toLowerCase() == stateparam
+    );
+  });
+  res.json({
+    massage: "filter by country and state",
+    result,
+  });
+});
 
-    let countryparam = String(req.params.country).toLowerCase()
-    let stateparam =String(req.params.state).toLowerCase()
+app.get("/country/state/city/:country/:state/:city", function (req, res) {
+  let countryparam = String(req.params.country).toLowerCase();
+  let stateparam = String(req.params.state).toLowerCase();
+  let cityparam = String(req.params.city).toLowerCase();
 
-    let result= data?.filter((w)=>{
-        return String(w.country).toLowerCase()==countryparam && String(w.state).toLowerCase()==stateparam
-    })
-    res.json({
-        massage:'filter by country and state',
-        result 
-    })
-
-})
-
-
-
-
-aap.get('/country/state/city/:country/:state/:city' , function (req, res) {
-
-    let countryparam = String(req.params.country).toLowerCase()
-    let stateparam = String(req.params.state).toLowerCase()
-    let cityparam =String(req.params.city).toLowerCase()
-
-    let result = data?.filter((w) => {
-        return String(w.country).toLowerCase() == countryparam && String(w.state).toLowerCase() == stateparam && String(W.city)==cityparam
-    })
-    res.json({
-        massage: 'filter by country and state',
-        result
-    })
-
-})
-
-
-
+  let result = data?.filter((w) => {
+    return (
+      String(w.country).toLowerCase() == countryparam &&
+      String(w.state).toLowerCase() == stateparam &&
+      String(W.city) == cityparam
+    );
+  });
+  res.json({
+    massage: "filter by country and state",
+    result,
+  });
+});
 
 var server = app.listen(8081, function () {
-    var host = server.address('localhost').address
-    var port = server.address().port
+  var host = server.address("localhost").address;
+  var port = server.address().port;
 
-    console.log("Example app listening at http://%s:%s", host, port)
-})
+  console.log("Example app listening at http://%s:%s", host, port);
+});
